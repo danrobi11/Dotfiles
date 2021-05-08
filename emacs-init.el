d45a5f9fde4c97ec97756b195aafe6541ef184b6
danrobi11 <48020114+danrobi11@users.noreply.github.com>
2021-05-08 18:21:35 -0400

    Update emacs-init.el
    new best 08/may/21

--- emacs-init.el
+++ emacs-init.el
@@ -9,7 +9,7 @@
 ;; http://ergoemacs.org/emacs/emacs.htmlhttps://melpa.org/
 ;; https://github.com/ch11ng/exwm/wiki/Configuration-Example
 ;; https://sachachua.com/dotemacs
-;; open 
+;; swap buffer position # window-swap-states
 ;; create new file = find file command
 ;; zoom it/out # text-scale-increase/decrease
 ;; install all the emacs icon # M-x all-the-icons-install-fonts
@@ -23,6 +23,9 @@
 ;; Press o to open a folder in a separate window
 ;; calendar # i-d add-a-dairy| d show-dairy | x show-holidays | m show-mark-days | u hide-mark-days-and-holidays | h show-holiday-description
 ;; themes | doom-homage-black | doom-moonlight | doom-challenger-deep
+;; --debug-init
+;; select all # C-x h
+;; (setq dashboard-banner-logo-title "🐧 Hello Master 🐧")
 (defun load-history-filename-element (file-regexp)
   (let* ((loads load-history)
    (load-elt (and loads (car loads))))
@@ -58,18 +61,18 @@
   :ensure t
   :init
   (persp-mode))
-(use-package doom-themes
-  :ensure t
-  :init
-  (load-theme 'doom-homage-black t))
-(use-package doom-modeline
-  :ensure t
-  :init
-  (doom-modeline-mode))
+;;(use-package doom-themes
+;;  :ensure t
+;;  :init
+;;  (load-theme 'doom-homage-black t))
+;;(use-package doom-modeline
+;;  :ensure t
+;;  :init
+;;  (doom-modeline-mode))
 (use-package counsel
   :ensure t
   :init
-  (counsel-mode))  
+  (counsel-mode))
 (use-package auto-package-update
   :ensure t)
 (use-package elpher
@@ -83,7 +86,6 @@
   :init      ;; tweak dashboard config before loading it
   (setq dashboard-set-heading-icons t)
   (setq dashboard-set-file-icons t)
-  (setq dashboard-banner-logo-title "🐧 Hello Master 🐧")
   (setq dashboard-startup-banner 'logo) ;; use standard emacs logo as banner
   ;;(setq dashboard-startup-banner "~/.emacs.d/emacs-dash.png")  ;; use custom image as banner
   (setq dashboard-center-content t) ;; set to 't' for centered content
@@ -93,13 +95,20 @@
   :ensure t
   :init
   (page-break-lines-mode))
-(use-package emoji-cheat-sheet-plus
-  :ensure t
-  :init )
-(use-package emojify
+;;(use-package emoji-cheat-sheet-plus
+;;  :ensure t
+;;  :init )
+;;(use-package emojify
+;;  :ensure t
+;;  :init
+;;  (global-emojify-mode 1))
+(use-package xclip
   :ensure t
   :init
-  (global-emojify-mode 1))
+  (xclip-mode))
+(use-package clipetty
+  :ensure t
+  :hook (after-init . global-clipetty-mode))
 ;;(use-package dmenu
 ;;  :ensure t
 ;;  :init )
@@ -107,6 +116,10 @@
   :ensure t
   :config
   (load (expand-file-name "/home/danrobi/.elfeed/elfeed.el")))
+;;
+;; transparency stuff
+;; (set-frame-parameter (selected-frame) 'alpha '(100 100))
+;; (add-to-list 'default-frame-alist '(alpha 100 100))
 ;;
 (debug-on-entry t)
 (scroll-bar-mode -1)
@@ -121,8 +134,8 @@
 (save-place-mode 1)
 (global-visual-line-mode 1)
 (global-hl-line-mode 1)
-(mouse-avoidance-mode 1)
 (ido-mode 1)
+;;(mouse-avoidance-mode 1)
 ;; (mode-line-debug-mode 1)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -133,10 +146,9 @@
  '(display-time-day-and-date t)
  '(display-time-format nil)
  '(display-time-use-mail-icon t)
- '(mouse-avoidance-mode (quote banish) nil (avoid))
+;; '(mouse-avoidance-mode 'banish nil (avoid))
  '(package-selected-packages
-   (quote
-    (multi-term emojify emoji-fontset dashboard elpher zoom vterm auto-package-update persp-mode helm elfeed doom-themes doom-modeline counsel)))
+   '(xclip clipetty multi-term emojify emoji-fontset dashboard elpher zoom auto-package-update persp-mode helm elfeed doom-themes doom-modeline counsel))
  '(window-min-height 6))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -147,7 +159,9 @@
 ;; keybinds
 ;;(global-set-key (kbd "\C-x\C-b") 'buffer-menu-other-window)
 ;;(global-set-key (kbd "\C-x\b") 'switch-to-buffer-other-window)
-(global-set-key (kbd "<C-tab>") 'other-window)
+;;(global-set-key (kbd "<C-a") 'other-window)
+;;(global-set-key (kbd "M-w") 'clipboard-yank)
+(global-set-key (kbd "C-y") 'clipboard-yank)
 (define-prefix-command 'z-map)
 (global-set-key (kbd "C-z") 'z-map)
 (define-key z-map (kbd "b") 'eww)
@@ -157,12 +171,18 @@
 (define-key z-map (kbd "j") 'bookmark-jump)
 (define-key z-map (kbd "d") 'bookmark-delete)
 (define-key z-map (kbd "z") 'buffer-menu-other-window)
+(define-key z-map (kbd "w") 'window-swap-states)
+(define-key z-map (kbd "a") 'other-window)
+(define-key z-map (kbd "g") 'minibuffer-keyboard-quit)
 ;;(define-key z-map (kbd "k") 'kill-this-buffer)
 ;;(bind-key "<C-tab>" 'other-window)
 ;;(global-set-key (kbd "C-q") 'kill-this-buffer)
 ;;(global-set-key (kbd "<f5>") (lambda() (interactive)(find-file "~/")))
 ;;(bind-key "<f2> <right>" 'windmove-right)
+;;
+;; How to defene keybind to open a specific file or command
 ;;(defun config-visit ()
 ;;  (interactive)
 ;;  (find-file "/home/danrobi/.emacs.d/init.el"))
 ;;(global-set-key (kbd "C-c d") 'config-visit)
+
